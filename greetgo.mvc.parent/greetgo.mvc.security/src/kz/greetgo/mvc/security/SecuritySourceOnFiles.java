531a87020ef2accb293f18af341ada720eb61cfd
msidorenko <msidorenko@greet-go.com>
2016-07-28 16:41:56 +0600

    version 0.0.18. Added implementation of getPrivateKeyBytes(...) method.

--- greetgo.mvc.parent/greetgo.mvc.security/src/kz/greetgo/mvc/security/SecuritySourceOnFiles.java
+++ greetgo.mvc.parent/greetgo.mvc.security/src/kz/greetgo/mvc/security/SecuritySourceOnFiles.java
@@ -1,76 +1,81 @@
 package kz.greetgo.mvc.security;
 
-import kz.greetgo.util.ServerUtil;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-public class SecuritySourceOnFiles extends AbstractSecuritySource {
+import kz.greetgo.util.ServerUtil;
 
+public class SecuritySourceOnFiles extends AbstractSecuritySource {
+  
   private final int keySize, blockSize;
   private final File privateKeyFile;
   private final File publicKeyFile;
   private final SecuritySourceConfig conf;
-
-  public SecuritySourceOnFiles(int keySize, int blockSize, 
-                               File privateKeyFile, File publicKeyFile,
-                               SecuritySourceConfig conf) {
+  
+  public SecuritySourceOnFiles(int keySize, int blockSize, File privateKeyFile, File publicKeyFile,
+      SecuritySourceConfig conf) {
     this.keySize = keySize;
     this.blockSize = blockSize;
     this.privateKeyFile = privateKeyFile;
     this.publicKeyFile = publicKeyFile;
     this.conf = conf;
   }
-
+  
   public SecuritySourceOnFiles(File privateKeyFile, File publicKeyFile, SecuritySourceConfig conf) {
     this(DEFAULT_KEY_SIZE, DEFAULT_BLOCK_SIZE, privateKeyFile, publicKeyFile, conf);
   }
-
+  
   public SecuritySourceOnFiles(File privateKeyFile, File publicKeyFile) {
-    this(DEFAULT_KEY_SIZE, DEFAULT_BLOCK_SIZE, privateKeyFile, publicKeyFile, new SecuritySourceConfigDefault());
+    this(DEFAULT_KEY_SIZE, DEFAULT_BLOCK_SIZE, privateKeyFile, publicKeyFile,
+        new SecuritySourceConfigDefault());
   }
-
+  
   @Override
   protected int getKeySize() {
     return keySize;
   }
-
+  
   @Override
   public int getBlockSize() {
     return blockSize;
   }
-
+  
   @Override
   protected byte[] getPrivateKeyBytes() {
-    return new byte[0];
+    return loadFile(privateKeyFile);
   }
-
+  
   @Override
   protected void setPrivateKeyBytes(byte[] bytes) {
     saveToFile(bytes, privateKeyFile);
   }
-
+  
   @Override
   protected byte[] getPublicKeyBytes() {
-    try {
-      ByteArrayOutputStream out = new ByteArrayOutputStream(getKeySize() * 5);
-      try (FileInputStream in = new FileInputStream(publicKeyFile)) {
-        ServerUtil.copyStreamsAndCloseIn(in, out);
-      }
-      return out.toByteArray();
-    } catch (Exception e) {
-      if (e instanceof RuntimeException) throw (RuntimeException) e;
-      throw new RuntimeException(e);
-    }
+    return loadFile(publicKeyFile);
   }
-
+  
   @Override
   protected void setPublicKeyBytes(byte[] bytes) {
     saveToFile(bytes, publicKeyFile);
   }
-
+  
+  private byte[] loadFile(File file) {
+    try {
+      ByteArrayOutputStream out = new ByteArrayOutputStream(getKeySize() * 5);
+      
+      try (FileInputStream in = new FileInputStream(file)) {
+        ServerUtil.copyStreamsAndCloseIn(in, out);
+      }
+      return out.toByteArray();
+    } catch (Exception e) {
+      if (e instanceof RuntimeException) throw (RuntimeException)e;
+      throw new RuntimeException(e);
+    }
+  }
+  
   private static void saveToFile(byte[] bytes, File file) {
     try {
       file.getParentFile().mkdirs();
@@ -78,16 +83,16 @@ public class SecuritySourceOnFiles extends AbstractSecuritySource {
         out.write(bytes);
       }
     } catch (Exception e) {
-      if (e instanceof RuntimeException) throw (RuntimeException) e;
+      if (e instanceof RuntimeException) throw (RuntimeException)e;
       throw new RuntimeException(e);
     }
   }
-
+  
   @Override
   protected boolean hasKeys() {
     return privateKeyFile.exists() && publicKeyFile.exists();
   }
-
+  
   @Override
   protected SecuritySourceConfig conf() {
     return conf;
