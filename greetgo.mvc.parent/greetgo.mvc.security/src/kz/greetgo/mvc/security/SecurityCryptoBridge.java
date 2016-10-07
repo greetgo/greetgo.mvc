40be883dfbd6cfa56dd86c45a6e686300b257d1f
msidorenko <msidorenko@greet-go.com>
2016-10-07 16:48:46 +0600

    Added more traces

--- greetgo.mvc.parent/greetgo.mvc.security/src/kz/greetgo/mvc/security/SecurityCryptoBridge.java
+++ greetgo.mvc.parent/greetgo.mvc.security/src/kz/greetgo/mvc/security/SecurityCryptoBridge.java
@@ -1,25 +1,31 @@
 package kz.greetgo.mvc.security;
 
-import kz.greetgo.mvc.interfaces.MvcTrace;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.security.InvalidKeyException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+
+import kz.greetgo.mvc.interfaces.MvcTrace;
+
 public class SecurityCryptoBridge implements SecurityCrypto {
-
+  
   public static MvcTrace trace = null;
-
+  
   private final SecuritySource securitySource;
-
+  
   public SecurityCryptoBridge(SecuritySource securitySource) {
     this.securitySource = securitySource;
   }
-
+  
   private static byte[] encryptBlock(byte[] bytes, SecuritySource securitySource) {
     try {
       Cipher cipher = securitySource.getCipher();
@@ -30,14 +36,14 @@ public class SecurityCryptoBridge implements SecurityCrypto {
       throw new RuntimeException(e);
     }
   }
-
+  
   private static byte[] decryptBlock(byte[] encryptedBytes, SecuritySource securitySource) {
     try {
-
+      
       Cipher cipher = securitySource.getCipher();
       cipher.init(Cipher.DECRYPT_MODE, securitySource.getPrivateKey());
       return cipher.doFinal(encryptedBytes);
-
+      
     } catch (BadPaddingException | IllegalBlockSizeException e) {
       if (trace != null) trace.trace("CP beg5btne3j", e);
       return null;
@@ -46,13 +52,13 @@ public class SecurityCryptoBridge implements SecurityCrypto {
       throw new RuntimeException(e);
     }
   }
-
+  
   private interface EncryptedData {
     void encryptAndSet(byte[] bytes, SecuritySource securitySource);
-
+    
     byte[] decryptAndGet(SecuritySource securitySource);
   }
-
+  
   private static EncryptedData createEncryptedData(byte[] bytes, SecuritySource securitySource) {
     if (bytes == null) return null;
     final EncryptedData ret;
@@ -64,39 +70,38 @@ public class SecurityCryptoBridge implements SecurityCrypto {
     ret.encryptAndSet(bytes, securitySource);
     return ret;
   }
-
-
+  
   private static class SmallBlock implements EncryptedData, Serializable {
-
+    
     byte[] encryptedBytes;
-
+    
     @Override
     public void encryptAndSet(byte[] bytes, SecuritySource securitySource) {
       encryptedBytes = encryptBlock(bytes, securitySource);
     }
-
+    
     @Override
     public byte[] decryptAndGet(SecuritySource securitySource) {
       return decryptBlock(encryptedBytes, securitySource);
     }
   }
-
+  
   private static class ManyBlocks implements EncryptedData, Serializable {
-
+    
     byte[] encryptedSymmetricKey;
-
+    
     final List<byte[]> blockList = new ArrayList<>();
-
+    
     @Override
     public void encryptAndSet(byte[] bytes, SecuritySource securitySource) {
       final byte[] symmetricKey = new byte[securitySource.getBlockSize()];
       securitySource.getRandom().nextBytes(symmetricKey);
-
+      
       encryptedSymmetricKey = encryptBlock(symmetricKey, securitySource);
-
+      
       writeToBlockList(blockList, symmetricKey, bytes);
     }
-
+    
     @Override
     public byte[] decryptAndGet(SecuritySource securitySource) {
       final byte[] symmetricKey = decryptBlock(encryptedSymmetricKey, securitySource);
@@ -104,10 +109,10 @@ public class SecurityCryptoBridge implements SecurityCrypto {
       return readFromBlockList(blockList, symmetricKey);
     }
   }
-
+  
   static byte[] readFromBlockList(List<byte[]> blockList, byte[] symmetricKey) {
     int bytesCount = 0;
-
+    
     for (final byte[] block : blockList) {
       final int blockLength = block.length;
       for (int j = 0; j < blockLength; j++) {
@@ -115,25 +120,25 @@ public class SecurityCryptoBridge implements SecurityCrypto {
       }
       bytesCount += blockLength;
     }
-
+    
     byte[] ret = new byte[bytesCount];
-
+    
     int filledCount = 0;
     for (byte[] block : blockList) {
       int blockLength = block.length;
       System.arraycopy(block, 0, ret, filledCount, blockLength);
       filledCount += blockLength;
     }
-
+    
     return ret;
   }
-
+  
   static void writeToBlockList(List<byte[]> blockList, byte[] symmetricKey, byte[] bytes) {
     int performedCount = 0;
-
+    
     final int bytesLength = bytes.length;
     final int symmetricKeyLength = symmetricKey.length;
-
+    
     while (performedCount < bytesLength) {
       int performBorder = performedCount + symmetricKeyLength;
       if (performBorder > bytesLength) performBorder = bytesLength;
@@ -147,86 +152,95 @@ public class SecurityCryptoBridge implements SecurityCrypto {
       performedCount = performBorder;
     }
   }
-
+  
   @Override
   public byte[] encrypt(byte[] bytes) {
     EncryptedData encryptedData = createEncryptedData(bytes, securitySource);
     if (encryptedData == null) return null;
-
+    
     ByteArrayOutputStream bOut = new ByteArrayOutputStream();
-
+    
     try (ObjectOutputStream oos = new ObjectOutputStream(bOut)) {
       oos.writeObject(encryptedData);
     } catch (IOException e) {
       if (trace != null) trace.trace("CP qw3h54htjfi", e);
       throw new RuntimeException(e);
     }
-
+    
     return bOut.toByteArray();
   }
-
-
+  
   @Override
   public byte[] decrypt(byte[] encryptedBytes) {
     if (encryptedBytes == null) return null;
-
+    
     ByteArrayInputStream bIn = new ByteArrayInputStream(encryptedBytes);
     try (ObjectInputStream ois = new ObjectInputStream(bIn)) {
-
-      final EncryptedData encryptedData = (EncryptedData) ois.readObject();
-
+      
+      final EncryptedData encryptedData = (EncryptedData)ois.readObject();
+      
       return encryptedData.decryptAndGet(securitySource);
-
+      
     } catch (IOException | ClassNotFoundException | ClassCastException e) {
       if (trace != null) trace.trace("CP wbrjdftdshs", e);
       return null;
     }
-
+    
   }
-
+  
   @Override
   public byte[] sign(byte[] bytes) {
     if (bytes == null) return null;
     try {
-
+      
       final byte[] hash1 = securitySource.getMessageDigest().digest(bytes);
-
+      
       Cipher cipher = securitySource.getCipher();
       cipher.init(Cipher.ENCRYPT_MODE, securitySource.getPrivateKey());
-
+      
       return cipher.doFinal(hash1);
-
+      
     } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
       if (trace != null) trace.trace("CP s2htvfyhdtew", e);
       throw new RuntimeException(e);
     }
   }
-
+  
   @Override
   public boolean verifySignature(byte[] bytes, byte[] signature) {
-    if (signature == null || bytes == null) return false;
+    if (signature == null || bytes == null) {
+      if (trace != null) trace.trace("CP bwhe4y56fghd");
+      return false;
+    }
     try {
-
+      
       final byte[] hash1 = securitySource.getMessageDigest().digest(bytes);
-
+      
       Cipher cipher = securitySource.getCipher();
       cipher.init(Cipher.DECRYPT_MODE, securitySource.getPublicKey());
-
+      
       final byte[] hash2 = cipher.doFinal(signature);
-
-      if (hash1.length != hash2.length) return false;
-
-      for (int i = 0, n = hash1.length; i < n; i++) {
-        if (hash1[i] != hash2[i]) return false;
+      
+      if (hash1.length != hash2.length) {
+        if (trace != null) trace.trace("CP 4frgfuibv3");
+        return false;
       }
-
+      
+      for (int i = 0, n = hash1.length; i < n; i++) {
+        if (hash1[i] != hash2[i]) {
+          if (trace != null) trace.trace("CP cye6tje3fo0hj");
+          return false;
+        }
+      }
+      
+      if (trace != null) trace.trace("CP 1f6cy7vyhrjjui");
       return true;
-
+      
     } catch (BadPaddingException | IllegalBlockSizeException | ArrayIndexOutOfBoundsException e) {
+      
       if (trace != null) trace.trace("CP w48shr3434e", e);
-
       return false;
-
+      
     } catch (InvalidKeyException e) {
       if (trace != null) trace.trace("CP q23hrbwh4g5", e);
       throw new RuntimeException(e);
--- greetgo.mvc.parent/greetgo.mvc.security/src/kz/greetgo/mvc/security/SecurityTunnelWrapper.java
+++ greetgo.mvc.parent/greetgo.mvc.security/src/kz/greetgo/mvc/security/SecurityTunnelWrapper.java
@@ -1,146 +1,149 @@
 package kz.greetgo.mvc.security;
 
+import static kz.greetgo.mvc.util.Base64Util.base64ToBytes;
+import static kz.greetgo.mvc.util.Base64Util.bytesToBase64;
+
+import java.util.Arrays;
+
 import kz.greetgo.mvc.interfaces.MvcTrace;
 import kz.greetgo.mvc.interfaces.RequestTunnel;
 import kz.greetgo.mvc.interfaces.TunnelHandler;
 import kz.greetgo.util.events.EventHandler;
 import kz.greetgo.util.events.HandlerKiller;
 
-import java.util.Arrays;
-
-import static kz.greetgo.mvc.util.Base64Util.base64ToBytes;
-import static kz.greetgo.mvc.util.Base64Util.bytesToBase64;
-
 public final class SecurityTunnelWrapper implements TunnelHandler {
-
+  
   private final TunnelHandler whatWrapping;
   private final SecurityProvider provider;
   private final SessionStorage sessionStorage;
   private final SecurityCrypto sessionCrypto;
   private final SecurityCrypto signatureCrypto;
-
+  
   public static MvcTrace trace;
-
+  
   public SecurityTunnelWrapper(TunnelHandler whatWrapping, SecurityProvider provider,
-                               SessionStorage sessionStorage, SecurityCrypto sessionCrypto,
-                               SecurityCrypto signatureCrypto) {
+      SessionStorage sessionStorage, SecurityCrypto sessionCrypto, SecurityCrypto signatureCrypto) {
     this.whatWrapping = whatWrapping;
     this.provider = provider;
     this.sessionStorage = sessionStorage;
     this.sessionCrypto = sessionCrypto;
     this.signatureCrypto = signatureCrypto;
   }
-
+  
   @Override
   public void handleTunnel(final RequestTunnel tunnel) {
     if (trace != null) trace.trace("CP HDBEwehrewh START!!!");
     final String target = tunnel.getTarget();
-
+    
     if (trace != null) trace.traceInTunnel("start target = " + target, tunnel);
-
+    
     if (provider.skipSession(target)) {
       if (trace != null) trace.trace("CP djsanjer3 skipSession");
       whatWrapping.handleTunnel(tunnel);
       return;
     }
-
+    
     final boolean underSecurityUmbrella = provider.isUnderSecurityUmbrella(target);
-
+    
     if (trace != null) trace.trace("underSecurityUmbrella = " + underSecurityUmbrella);
-
+    
     final byte[] bytesInStorage;
-
+    
     {
       final String sessionBase64 = tunnel.cookies().getFromRequest(provider.cookieKeySession());
       byte[] bytes = base64ToBytes(sessionBase64);
-
-      if (trace != null) trace.trace("CP HEBWJD bytes " + (bytes == null ? "is null" : " is not null"));
-
+      
+      if (trace != null) trace.trace("CP HEBWJD bytes "
+          + (bytes == null ? "is null" :" is not null"));
+      
       if (sessionCrypto != null) {
         if (trace != null) trace.trace("CP GRHWVe sessionCrypto != null");
         bytes = sessionCrypto.decrypt(bytes);
       }
-
+      
       if (underSecurityUmbrella && bytes != null && signatureCrypto != null) {
         if (trace != null) trace.trace("CP QTTSFrt");
         String signatureBase64 = tunnel.cookies().getFromRequest(provider.cookieKeySignature());
+        if (trace != null) trace.trace("CP h2hrhbrfer signatureBase64 == null -> "
+            + (signatureBase64 == null));
         byte[] signature = base64ToBytes(signatureBase64);
         if (!signatureCrypto.verifySignature(bytes, signature)) {
           if (trace != null) trace.trace("CP QKMRTBG bytes := null");
           bytes = null;
         }
       }
-
+      
       if (trace != null) trace.trace("CP hrebtrhet");
       sessionStorage.setSessionBytes(bytesInStorage = bytes);
     }
-
+    
     final EventHandler writeSessionToCookies = new EventHandler() {
       boolean performed = false;
-
+      
       @Override
       public void handle() {
         if (performed) return;
         performed = true;
-
+        
         if (trace != null) trace.trace("CP h6h34vyt");
-
+        
         byte[] bytes = sessionStorage.getSessionBytes();
-        if (trace != null) trace.trace("CP 543nj654 " + (bytes == null ? "bytes is null" : "bytes is not null"));
-
+        if (trace != null) trace.trace("CP 543nj654 "
+            + (bytes == null ? "bytes is null" :"bytes is not null"));
+        
         if (Arrays.equals(bytesInStorage, bytes)) {
           if (trace != null) trace.trace("CP krmet54 EQUALS");
           return;
         }
-
+        
         if (trace != null) trace.trace("CP ktmreyr");
-
+        
         if (bytes == null) {
           if (trace != null) trace.trace("CP uyu76gfh4 bytes == null");
           tunnel.cookies().removeFromResponse(provider.cookieKeySession());
           tunnel.cookies().removeFromResponse(provider.cookieKeySignature());
         } else {
-
+          
           if (trace != null) trace.trace("CP thbrejhby bytes != null");
-
+          
           if (signatureCrypto != null) {
             byte[] signature = signatureCrypto.sign(bytes);
             final String signatureBase64 = bytesToBase64(signature);
             if (trace != null) trace.trace("CP tytgfyr save signature");
             tunnel.cookies().saveToResponse(provider.cookieKeySignature(), signatureBase64);
           }
-
+          
           if (sessionCrypto != null) {
             if (trace != null) trace.trace("CP yfc56g34kjd sessionCrypto != null");
             bytes = sessionCrypto.encrypt(bytes);
           }
-
+          
           final String bytesBase64 = bytesToBase64(bytes);
           if (trace != null) trace.trace("CP vv4t5v43t save bytesBase64");
           tunnel.cookies().saveToResponse(provider.cookieKeySession(), bytesBase64);
-
+          
         }
-
+        
       }
     };
-
+    
     final HandlerKiller handlerKiller = tunnel.eventBeforeCompleteHeaders().addEventHandler(
         writeSessionToCookies);
-
+    
     if (trace != null) trace.trace("CP bgeneju478r");
-
+    
     try {
-
+      
       if (!underSecurityUmbrella) {
-
+        
         if (trace != null) trace.trace("CP nsbhryhfbv");
-
+        
         whatWrapping.handleTunnel(tunnel);
-
+        
         if (trace != null) trace.trace("CP jen4676v4g");
-
+        
       } else {
-
+        
         if (sessionStorage.getSessionBytes() == null) {
           if (trace != null) trace.trace("CP hytrjtreh redirect");
           tunnel.sendRedirect(provider.redirectOnSecurityError(target));
@@ -148,14 +151,14 @@ public final class SecurityTunnelWrapper implements TunnelHandler {
           if (trace != null) trace.trace("CP fbdvfgd EXECUTE");
           whatWrapping.handleTunnel(tunnel);
         }
-
+        
       }
-
+      
     } finally {
       handlerKiller.kill();
       writeSessionToCookies.handle();
       if (trace != null) trace.trace("CP fjewhtb FINALLY");
     }
-
+    
   }
 }
