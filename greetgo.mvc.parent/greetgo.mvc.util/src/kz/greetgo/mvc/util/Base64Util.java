e6a8f9795def86d789d811439c1a4cb4eb8e700a
msidorenko <msidorenko@greet-go.com>
2016-10-10 11:03:12 +0600

    version 0.0.26 Source compatability moved to 1.8. Changed encode and decode methods in Base64Util to use java.util.Base64 and its UrlEncoder. fixed failing tests.

--- greetgo.mvc.parent/greetgo.mvc.util/src/kz/greetgo/mvc/util/Base64Util.java
+++ greetgo.mvc.parent/greetgo.mvc.util/src/kz/greetgo/mvc/util/Base64Util.java
@@ -1,12 +1,12 @@
 package kz.greetgo.mvc.util;
 
-import javax.xml.bind.DatatypeConverter;
+import java.util.Base64;
 
 public class Base64Util {
   public static byte[] base64ToBytes(String base64) {
     if (base64 == null) return null;
     try {
-      final byte[] ret = DatatypeConverter.parseBase64Binary(base64);
+      final byte[] ret = Base64.getUrlDecoder().decode(base64);
       if (ret == null) return null;
       if (ret.length == 0) return null;
       return ret;
@@ -14,9 +14,9 @@ public class Base64Util {
       return null;
     }
   }
-
+  
   public static String bytesToBase64(byte[] bytes) {
     if (bytes == null) return null;
-    return DatatypeConverter.printBase64Binary(bytes);
+    return Base64.getUrlEncoder().encodeToString(bytes);
   }
 }
--- greetgo.mvc.parent/greetgo.mvc/test_src/kz/greetgo/mvc/model/RedirectTest.java
+++ greetgo.mvc.parent/greetgo.mvc/test_src/kz/greetgo/mvc/model/RedirectTest.java
@@ -1,21 +1,20 @@
 package kz.greetgo.mvc.model;
 
-import kz.greetgo.mvc.util.CookieUtil;
-import org.testng.annotations.Test;
-
 import static org.fest.assertions.api.Assertions.assertThat;
 
-public class RedirectTest {
+import org.testng.annotations.Test;
 
+public class RedirectTest {
+  
   @Test
   public void addCookie() {
     //noinspection ThrowableResultOfMethodCallIgnored
-    final Redirect redirect = Redirect.to("some reference")
-      .addCookie("asd", "asd value").addCookie("dsa", "dsa object value");
-
+    final Redirect redirect = Redirect.to("some reference").addCookie("asd", "asd value")
+        .addCookie("dsa", "dsa object value");
+    
     assertThat(redirect.reference).isEqualTo("some reference");
     assertThat(redirect.savingCookiesToResponse.get("asd")).isEqualTo("asd value");
-    assertThat(redirect.savingCookiesToResponse.get("dsa")).isEqualTo(CookieUtil.objectToStr("dsa object value"));
+    assertThat(redirect.savingCookiesToResponse.get("dsa")).isEqualTo("dsa object value");
   }
-
+  
 }
