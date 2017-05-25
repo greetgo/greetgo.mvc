42911bd47323c0c7b83665375cf162ea0b1d8315
ekolpakov <ekolpakov@greet-go.com>
2017-05-25 15:37:32 +0600

    version = '1.0.2' - добавлена возможность конвертирования строки в енум

--- greetgo.mvc.parent/greetgo.mvc.war/src/kz/greetgo/mvc/war/SecurityFilter.java
+++ greetgo.mvc.parent/greetgo.mvc.war/src/kz/greetgo/mvc/war/SecurityFilter.java
@@ -1,7 +1,6 @@
 package kz.greetgo.mvc.war;
 
 import kz.greetgo.mvc.interfaces.MvcTrace;
-import kz.greetgo.mvc.interfaces.RequestTunnel;
 import kz.greetgo.mvc.interfaces.TunnelHandler;
 import kz.greetgo.mvc.security.SecurityCrypto;
 import kz.greetgo.mvc.security.SecurityProvider;
@@ -46,15 +45,16 @@ public abstract class SecurityFilter implements Filter {
   public void doFilter(final ServletRequest request, final ServletResponse response,
                        final FilterChain chain) throws IOException, ServletException {
 
-    if (trace != null) trace.trace("SF hew3jbd6s344h");
+    if (trace != null) trace.trace("SF hew3jbd6s344h Started security doFilter");
 
     final WarRequestTunnel tunnel = new WarRequestTunnel(request, response);
 
     final TunnelHandler chainHandler = handlerTunnel -> {
       try {
-        if (trace != null) trace.trace("SF kdm4sde1urt");
+        if (trace != null) trace.trace("SF kdm4sde1urt started chainHandler");
         request.setAttribute(ATTRIBUTE_TUNNEL, handlerTunnel);
         chain.doFilter(request, response);
+        if (trace != null) trace.trace("SF nsg3r4gD finished chainHandler");
       } catch (ServletException | IOException e) {
         if (trace != null) trace.trace("SF gds5vgh4ewg", e);
         throw new ExceptionWrapper(e);
@@ -62,12 +62,13 @@ public abstract class SecurityFilter implements Filter {
     };
 
     final SecurityTunnelWrapper stw = new SecurityTunnelWrapper(chainHandler, getProvider(),
-        getSessionStorage(), getSessionCrypto(), getSignatureCrypto());
+      getSessionStorage(), getSessionCrypto(), getSignatureCrypto());
 
     try {
       stw.handleTunnel(tunnel);
+      if (trace != null) trace.trace("SF gw263vrf2ex Finished security doFilter");
     } catch (ExceptionWrapper e) {
-      if (trace != null) trace.trace("SF dbh1sbe2wr456e", e);
+      if (trace != null) trace.trace("SF dbh1sbe2wr456e SecurityTunnelWrapper exception", e);
 
       if (e.wrappedException instanceof IOException) throw (IOException) e.wrappedException;
       if (e.wrappedException instanceof ServletException) throw (ServletException) e.wrappedException;
