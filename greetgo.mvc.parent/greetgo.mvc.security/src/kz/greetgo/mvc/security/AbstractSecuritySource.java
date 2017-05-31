60f9c11e0518a7b5296c41aaccb4ff17c9b73e5e
ekolpakov <ekolpakov@greet-go.com>
2017-05-31 13:01:51 +0600

    version=1.0.3.1 - Исправлен AbstractSecuritySource - теперь, если не можно считать ключи, то генерируются новые

--- greetgo.mvc.parent/greetgo.mvc.security/src/kz/greetgo/mvc/security/AbstractSecuritySource.java
+++ greetgo.mvc.parent/greetgo.mvc.security/src/kz/greetgo/mvc/security/AbstractSecuritySource.java
@@ -1,6 +1,7 @@
 package kz.greetgo.mvc.security;
 
-import java.io.FileNotFoundException;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -13,35 +14,32 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
-import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
-
 public abstract class AbstractSecuritySource implements SecuritySource {
-  
+
   public static final int DEFAULT_KEY_SIZE = 1024;
   public static final int DEFAULT_BLOCK_SIZE = 117;
-  
+
   protected int getKeySize() {
     return DEFAULT_KEY_SIZE;
   }
-  
+
   protected abstract byte[] getPrivateKeyBytes();
-  
+
   protected abstract void setPrivateKeyBytes(byte[] bytes);
-  
+
   protected abstract byte[] getPublicKeyBytes();
-  
+
   protected abstract void setPublicKeyBytes(byte[] bytes);
-  
+
   protected abstract boolean hasKeys();
-  
+
   protected abstract SecuritySourceConfig conf();
-  
+
   @Override
   public int getBlockSize() {
     return DEFAULT_BLOCK_SIZE;
   }
-  
+
   @Override
   public Cipher getCipher() {
     try {
@@ -50,38 +48,33 @@ public abstract class AbstractSecuritySource implements SecuritySource {
       throw new RuntimeException(e);
     }
   }
-  
-  private final ThreadLocal<PublicKey> publicKey = new ThreadLocal<PublicKey>();
-  private final ThreadLocal<PrivateKey> privateKey = new ThreadLocal<PrivateKey>();
-  
+
+  private final ThreadLocal<PublicKey> publicKey = new ThreadLocal<>();
+  private final ThreadLocal<PrivateKey> privateKey = new ThreadLocal<>();
+
   private void prepareKeys() {
     if (privateKey.get() != null && publicKey.get() != null) return;
-    
+
     synchronized (this) {
       if (privateKey.get() != null && publicKey.get() != null) return;
-      try {
-        doPrepareKeys();
-      } catch (Exception e) {
-        if (e instanceof RuntimeException) throw (RuntimeException)e;
-        throw new RuntimeException(e);
-      }
+      doPrepareKeys();
     }
   }
-  
+
   @Override
   public PublicKey getPublicKey() {
     prepareKeys();
     return publicKey.get();
   }
-  
+
   @Override
   public PrivateKey getPrivateKey() {
     prepareKeys();
     return privateKey.get();
   }
-  
-  private final ThreadLocal<SecureRandom> random = new ThreadLocal<SecureRandom>();
-  
+
+  private final ThreadLocal<SecureRandom> random = new ThreadLocal<>();
+
   @Override
   public SecureRandom getRandom() {
     if (random.get() != null) return random.get();
@@ -93,16 +86,16 @@ public abstract class AbstractSecuritySource implements SecuritySource {
       throw new RuntimeException(e);
     }
   }
-  
-  private final ThreadLocal<MessageDigest> messageDigest = new ThreadLocal<MessageDigest>();
-  
+
+  private final ThreadLocal<MessageDigest> messageDigest = new ThreadLocal<>();
+
   @Override
   public MessageDigest getMessageDigest() {
     {
       MessageDigest result = messageDigest.get();
       if (result != null) return result;
     }
-    
+
     try {
       MessageDigest instance = MessageDigest.getInstance(conf().messageDigestAlgorithm());
       messageDigest.set(instance);
@@ -111,20 +104,30 @@ public abstract class AbstractSecuritySource implements SecuritySource {
       throw new RuntimeException(e);
     }
   }
-  
-  private void doPrepareKeys() throws Exception {
+
+  protected void performReadingException(Exception e) {
+    e.printStackTrace();
+  }
+
+  private void doPrepareKeys() {
     if (hasKeys()) {
-      readKeysFromFiles();
-    } else {
+      try {
+        readKeysFromFiles();
+        return;
+      } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+        performReadingException(e);
+      }
+    }
+    {
       generateKeys();
       saveKeys();
     }
   }
-  
+
   private void saveKeys() {
     {
       final PKCS8EncodedKeySpec privateKetSpec = new PKCS8EncodedKeySpec(privateKey.get()
-          .getEncoded());
+        .getEncoded());
       setPrivateKeyBytes(privateKetSpec.getEncoded());
     }
     {
@@ -132,37 +135,34 @@ public abstract class AbstractSecuritySource implements SecuritySource {
       setPublicKeyBytes(publicSpec.getEncoded());
     }
   }
-  
+
   private void generateKeys() {
     try {
-      
+
       final KeyPairGenerator kpg = KeyPairGenerator.getInstance(conf().keyPairGeneratorAlgorithm());
-      
+
       kpg.initialize(getKeySize(), getRandom());
-      
+
       final KeyPair keyPair = kpg.generateKeyPair();
-      
+
       privateKey.set(keyPair.getPrivate());
       publicKey.set(keyPair.getPublic());
-      
+
     } catch (NoSuchAlgorithmException e) {
       throw new RuntimeException(e);
     }
   }
-  
-  private void readKeysFromFiles() throws FileNotFoundException {
-    try {
-      
-      final PKCS8EncodedKeySpec keySpecPrivate = new PKCS8EncodedKeySpec(getPrivateKeyBytes());
-      final X509EncodedKeySpec keySpecPublic = new X509EncodedKeySpec(getPublicKeyBytes());
-      
-      final KeyFactory keyFactory = KeyFactory.getInstance(conf().keyFactoryAlgorithm());
-      
-      privateKey.set(keyFactory.generatePrivate(keySpecPrivate));
-      publicKey.set(keyFactory.generatePublic(keySpecPublic));
-      
-    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-      throw new RuntimeException(e);
-    }
+
+  private void readKeysFromFiles() throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+
+    final PKCS8EncodedKeySpec keySpecPrivate = new PKCS8EncodedKeySpec(getPrivateKeyBytes());
+    final X509EncodedKeySpec keySpecPublic = new X509EncodedKeySpec(getPublicKeyBytes());
+
+    final KeyFactory keyFactory = KeyFactory.getInstance(conf().keyFactoryAlgorithm());
+
+    privateKey.set(keyFactory.generatePrivate(keySpecPrivate));
+    publicKey.set(keyFactory.generatePublic(keySpecPublic));
+
   }
 }
